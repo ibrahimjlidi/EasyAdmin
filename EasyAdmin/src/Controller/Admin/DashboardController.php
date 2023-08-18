558ef45fef4a2b64166340236a014d6b5796ec9a
ibrahim <ibrahimljlidi23@gmail.com>
2023-08-18 17:07:36 +0200

    login+fixtures

--- EasyAdmin/src/Controller/Admin/DashboardController.php
+++ EasyAdmin/src/Controller/Admin/DashboardController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Admin;
 
 use App\Entity\Category;
 use App\Entity\Product;
-use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -28,7 +27,6 @@ class DashboardController extends AbstractDashboardController
         $url = $this->adminUrlGenerator
         ->setController(ProductCrudController::class)
         ->generateUrl();
-        
         // return parent::index();
 
         return $this->redirect($url);
@@ -38,17 +36,11 @@ class DashboardController extends AbstractDashboardController
         return Crud::new()
             ->renderContentMaximized()
             -> showEntityActionsInlined();
-            
-            
     }
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Website')
-            ->setLocales([
-                'en' => 'EN English',
-                'fr' => 'FR France'
-            ]);
+            ->setTitle('Website');
     }
 
     public function configureMenuItems(): iterable
@@ -63,11 +55,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
             MenuItem::linkToCrud('create category', 'fas fa-plus',Category::class)->setAction(Crud::PAGE_NEW),
             MenuItem::linkToCrud('show Categories', 'fas fa-eye',Category::class)
-        ]);
-        yield MenuItem::section('Users');
-        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
-            MenuItem::linkToCrud('create user', 'fas fa-plus',User::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('show users', 'fas fa-eye',User::class)
         ]);
 
     }
